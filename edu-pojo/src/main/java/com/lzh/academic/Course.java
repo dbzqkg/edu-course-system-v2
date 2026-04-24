e56f744b152a4c6dd1f68fe882fd96310a869d1f
林子涵 <3233500864@qq.com>
2026-04-24 23:25:02 +0800

    1

---
+++ edu-pojo/src/main/java/com/lzh/academic/Course.java
@@ -0,0 +1,31 @@
+package com.lzh.academic;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("edu_course")
+public class Course implements Serializable {
+    @TableId(type = IdType.INPUT)
+    private Long id;
+    private String courseNo;
+    private String name;
+    private BigDecimal credits;
+    private Integer courseType;
+    private String tagSet;
+    
+    @Version
+    private Integer version;
+}
---
+++ edu-pojo/src/main/java/com/lzh/audit/SelectionLog.java
@@ -0,0 +1,30 @@
+package com.lzh.audit;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("edu_selection_log")
+public class SelectionLog implements Serializable {
+    @TableId(type = IdType.INPUT)
+    private Long id;
+    private Long studentId;
+    private Long classId;
+    private Integer operationType;
+    private Integer selectionStatus;
+    private String failReason;
+    private String traceId;
+    private Integer duration;
+    private LocalDateTime createTime;
+}
